53af711c8cc605567430d51fe4646f72e9216034
Brandon Renk <renkb34@gmail.com>
2024-01-17 15:00:37 -0500

    Database successfully runs & sample data added

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,43 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO person (first_name, last_name)
+VALUES ('Stan', 'Lee'),
+    ('Louise Jones', 'Simonson'),
+    ('Scott', 'Lobdell'),
+    ('Grant', 'Morrison'),
+    ('Christopher', 'Priest'),
+    ('John', 'Buscema'),
+    ('Jon', 'Bogdanove'),
+    ('Joe', 'Madureira'),
+    ('Jill', 'Thompson'),
+    ('John', 'Ross'),
+    ('Glenn', 'Whitmore'),
+    ('Steve', 'Buccellato'),
+    ('Daniel', 'Vozzo'),
+    ('Trish', 'Mulvihill'),
+    ('Mike', 'Carlin'),
+    ('Ben', 'Raab'),
+    ('Stuart', 'Moore'),
+    ('Paul', 'Kupperberg'),
+    ('Chic', 'Stone'),
+    ('Bob', 'McLeod'),
+    ('Dan', 'Green'),
+    ('Dennis', 'Cramer'),
+    ('Romeo', 'Tanghal'),
+    ('Sam', 'Rosen'),
+    ('Bill', 'Oakley'),
+    ('Chris', 'Eliopoulos'),
+    ('Clem', 'Robins');
+
+INSERT INTO comics (title, cover_img, volume, issue_number, cover_date, writer_id, artist_id, colorist_id, editor_id, inker_id, letterer_id)
+VALUES ('In The Hands of ... Mephisto!', 'https://comicvine.gamespot.com/a/uploads/scale_large/11/117763/2403520-ss16.png', 'The Silver Surfer', 16, '05/01/1970', 1, 6, NULL, 1, 19, 24),
+    ('The Underworld Rises Again', 'https://comicvine.gamespot.com/a/uploads/scale_large/0/4/32659-4571-36430-1-superman-the-man-of.jpg', 'Superman: The Man of Steel', 17, '11/01/1992', 2, 7, 11, 15, 20 , 25),
+    ('Generation Next, Part 3: Enter Freely And of Your Own Will', 'https://comicvine.gamespot.com/a/uploads/scale_large/0/4/35672-3092-39830-1-uncanny-x-men-the.jpg', 'The Uncanny X-Men', 317, '10/01/1994', 3, 8, 12, 16, 21, 26),
+    ('Arcadia, Part 3: 120 Days of Sod All', 'https://comicvine.gamespot.com/a/uploads/scale_large/6/67663/4404810-07.jpg', 'The Invisibles', 7, '03/01/1995', 4, 9, 13, 17, 22, 27),
+    ('Home', 'https://comicvine.gamespot.com/a/uploads/scale_large/0/4/46617-3824-55260-1-wonder-woman.jpg', 'Wonder Woman', 89, '08/01/1994', 5, 10, 14, 18, 23, 27);
+
+
+
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users, comics, user_comic, comic_collections, user_collections, writers, artists CASCADE;
+DROP TABLE IF EXISTS users, person, comics, user_comic, user_collections, comic_user_collection CASCADE;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -10,64 +10,56 @@ CREATE TABLE users (
 
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+CREATE TABLE person (
+	person_id SERIAL,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+
+	CONSTRAINT PK_person PRIMARY KEY (person_id)
+);
 CREATE TABLE comics (
 	comic_id SERIAL,
-	title varchar(50) NOT NULL,
+	title varchar(200) NOT NULL,
 	cover_img varchar(200),
+	volume varchar(50) NOT NULL,
 	issue_number int NOT NULL,
-	number_pages int NOT NULL,
-	date_published DATE NOT NULL,
-	writer1_id int NOT NULL,
-	writer2_id int,
-	writer3_id int,
-	artist1_id int NOT NULL,
-	artist2_id int,
-	artist3_id int,
+	cover_date DATE NOT NULL,
+	writer_id int NULL,
+	artist_id int NULL,
+	colorist_id int NULL,
+	editor_id int NULL,
+	inker_id int NULL,
+	letterer_id int NULL,
+
 
 	CONSTRAINT PK_comics PRIMARY KEY (comic_id),
-	CONSTRAINT FK_writers FOREIGN KEY (writer1_id) REFERENCES writers(writer_id),
-	CONSTRAINT FK_writers FOREIGN KEY (writer2_id) REFERENCES writers(writer_id),
-	CONSTRAINT FK_writers FOREIGN KEY (writer3_id) REFERENCES writers(writer_id),
-	CONSTRAINT FK_artists FOREIGN KEY (artist1_id) REFERENCES artists(artist_id),
-	CONSTRAINT FK_artists FOREIGN KEY (artist2_id) REFERENCES artists(artist_id),
-	CONSTRAINT FK_artists FOREIGN KEY (artist3_id) REFERENCES artists(artist_id),
+	CONSTRAINT FK_comics_writer FOREIGN KEY (writer_id) REFERENCES person(person_id),
+	CONSTRAINT FK_comics_artist FOREIGN KEY (artist_id) REFERENCES person(person_id),
+	CONSTRAINT FK_comics_colorist FOREIGN KEY (colorist_id) REFERENCES person(person_id),
+	CONSTRAINT FK_comics_editor FOREIGN KEY (editor_id) REFERENCES person(person_id),
+	CONSTRAINT FK_comics_inker FOREIGN KEY (inker_id) REFERENCES person(person_id),
+	CONSTRAINT FK_comics_letterer FOREIGN KEY (letterer_id) REFERENCES person(person_id)
 );
 CREATE TABLE user_comic (
 	user_id int NOT NULL,
-	comic_id int NOT NULL
+	comic_id int NOT NULL,
 
-	CONSTRAINT PK_user_comic PRIMARY KEY (user_id),
-	CONSTRAINT PK_user_comic PRIMARY KEY (comic_id)
-
+	CONSTRAINT PK_user_comic PRIMARY KEY (user_id, comic_id)
 );
 CREATE TABLE user_collections (
 	user_collection_id SERIAL,
 	collection_name varchar(100),
-	user_id int NOT NULL
+	user_id int NOT NULL,
 
 	CONSTRAINT PK_user_collections PRIMARY KEY (user_collection_id),
 	CONSTRAINT FK_user_collections FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-CREATE TABLE comic_collections (
+CREATE TABLE comic_user_collection (
+--    collection_id
 	comic_id int NOT NULL,
-	user_collection_id int NOT NULL
+	user_collection_id int NOT NULL,
 
-	CONSTRAINT PK_comic_collections PRIMARY KEY (comic_id),
-	CONSTRAINT PK_comic_collections PRIMARY KEY (user_collection_id)
-);
-CREATE TABLE writers (
-	writer_id SERIAL,
-	first_name varchar(30) NOT NULL,
-	last_name varchar(30) NOT NULL
-
-	CONSTRAINT PK_writers PRIMARY KEY (writer_id)
-);
-CREATE TABLE artists (
-	artist_id SERIAL,
-	first_name varchar(30) NOT NULL,
-	last_name varchar(30) NOT NULL
-
-	CONSTRAINT PK_artists PRIMARY KEY (artist_id)
+	CONSTRAINT PK_comic_user_collection PRIMARY KEY (comic_id, user_collection_id)
 );
 
 COMMIT TRANSACTION;
