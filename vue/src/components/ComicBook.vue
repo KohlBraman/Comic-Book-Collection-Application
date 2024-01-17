9cc505948b989a85a4858c9cc5cb08415da3fbb2
Alexander Chang <thenavyseal123@gmail.com>
2024-01-17 09:58:12 -0500

    final cap update 2

---
+++ vue/src/components/ComicBook.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>Comic Book Details</h2>
+    <!-- Display basic information about a comic book -->
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      comic: {} 
+    };
+  },
+  created() {
+    // Fetch comic data using an API call or other methods
+    const comicId = this.$route.params.id;
+    
+    this.fetchComicDetails(comicId);
+  },
+  methods: {
+    fetchComicDetails(comicId) {
+      // Make an API call to fetch comic details
+      this.$store.dispatch('comics/fetchComicDetails', comicId)
+        .then(comic => {
+          this.comic = comic;
+        })
+        .catch(error => {
+          console.error('Error fetching comic details:', error);
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
