dea39e3a193a7636cb866f73b45d7e4883e3fc57
Kohl Braman <kohlbraman@gmail.com>
2024-01-22 16:36:16 -0700

    Added User sidebar cleaning up the profile page. Implemented clickable profile picture to change it.

--- vue/src/components/UserPage.vue
+++ vue/src/components/UserPage.vue
@@ -1,184 +1,184 @@
 <template>
-  <div>
     <div class="welcome-box">
       <h1>Welcome to your profile!</h1>
     </div>
     <div class="comic-collection">
       <ComicCollection />
     </div>
+  
     <div class="main-container">
+  
       <div class="profile-container">
+  
         <div class="content-box">
-          <div class="icon-choice-box">
+          <p class="comic-username">{{ userName }}</p>
+          <!-- Clickable Profile Icon -->
+          <div class="icon-choice-box" @click="toggleProfileDropdown">
             <div id="ProfileIcon" class="profile-icon">
-              <p class="comic-username">{{ userName }}</p>
-              <img src="../assets/superman.jpeg" alt="profile picture placeholder">
+              <img :src="selectedProfilePicture" alt="profile picture placeholder">
             </div>
-            <div id="ProfilePrompt">
-              <p>{{ profilePrompt }}</p>
-              <button v-show="!showUpdatePrompt" @click="toggleUpdatePrompt">Update Prompt</button>
-              <div v-show="showUpdatePrompt" class="update-prompt-content">
-                <input v-model="newProfilePrompt" type="text" placeholder="Type a new prompt">
-                <button @click="updateProfilePrompt">Update Prompt</button>
-                <button @click="cancelUpdatePrompt">Cancel</button>
-              </div>
+            <!-- Dropdown for Profile Pictures -->
+            <div v-show="showProfileDropdown" class="profile-dropdown">
+              <select v-model="selectedProfilePicture" @click.stop>
+                <option v-for="picture in profilePictures" :key="picture" :value="picture">
+                  {{ picture }}
+                </option>
+              </select>
             </div>
           </div>
-        </div>
-        <div class="update-profile-box">
-          <button @click="toggleDropdown" v-show="!showDropdown">Update Profile</button>
-          <div v-show="showDropdown" class="dropdown-content">
-            <UpdateProfile />
-            <button @click="cancelUpdate" v-show="showDropdown">Cancel</button>
-          </div>
-        </div>
-
-        <!-- AddComic component placement -->
-        <div class="add-comic-box">
-          <AddComic @add-comic="handleAddComic" />
         </div>
       </div>
     </div>
-  </div>
-</template>
-
-<script>
-import UpdateProfile from '../components/UpdateProfile.vue';
-import ComicCollection from '../components/ComicCollection.vue';
-import AddComic from '../components/AddComic.vue';
-export default {
-  data() {
-  return {
-    showDropdown: false,
-    showUpdatePrompt: false,
-    newProfilePrompt: '',
-    profilePrompt: "Which mask do you wear?",
-    userName: "USER",
-    newUserName: '',
-    showUpdateUserName: false,
-  };
-},
-methods: {
-  toggleDropdown() {
-    this.showDropdown = !this.showDropdown;
-  },
-  toggleUpdatePrompt() {
-    this.showUpdatePrompt = !this.showUpdatePrompt;
-  },
-  cancelUpdate() {
-    this.showDropdown = false;
-  },
-  cancelUpdatePrompt() {
-    this.showUpdatePrompt = false;
-  },
-  updateProfilePrompt() {
-    if (this.newProfilePrompt) {
-      this.profilePrompt = this.newProfilePrompt;
-      this.newProfilePrompt = '';
-      this.showUpdatePrompt = false;
-    }
-  },
-  handleAddComic(newComic) {
-      // You can add logic here to update your comic collection with the new comic
-      console.log('New Comic Added:', newComic);
-      // For example, you might want to add it to an array in your data
-      // this.comics.push(newComic);
+    <!-- AddComic component placement -->
+    <div class="add-comic-box">
+      <AddComic @add-comic="handleAddComic" />
+    </div>
+  </template>
+    
+  <script>
+  import UpdateProfile from '../components/UpdateProfile.vue';
+  import ComicCollection from '../components/ComicCollection.vue';
+  import AddComic from '../components/AddComic.vue';
+  
+  export default {
+    data() {
+      return {
+  
+        userName: "USERNAME",
+        selectedProfilePicture: "../assets/superman.jpeg", // Default profile picture
+        profilePictures: [
+          "../assets/superman.jpeg",
+          "../assets/batman.jpeg",
+          "../assets/spiderman.jpeg",
+          // Add more image paths as needed
+        ],
+        showProfileDropdown: false,
+      };
     },
-
-},
-components: {
-  UpdateProfile,
-  ComicCollection,
-  AddComic,
-
-},
-}
-</script>
-<style scoped>
-.comic-collection {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  /* border: solid black; */
-  max-width: 800px;
- margin: 0 auto;
-}
-.main-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  margin-top: -300px;
-}
-.comic-username {
-font-size: 30px;
-font-weight: bold;
-font-family: 'Comic Sans MS', cursive;
-color: #0C0C0C;
-}
-.profile-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 100px;
-}
-#profileIcon {
-  width: 300px;
-height: 300px;
-border-radius: 100%;
-overflow: hidden;
-}
-.profile-icon img {
-width: 300px; /* Adjust the width */
-height: 250px; /* Adjust the height */
-}
-#ProfilePrompt {
-font-size: 25px;
-font-weight: bold;
-text-align: left;
-margin-bottom: 20px;
-}
-button {
-background-color: rgba(255, 0, 0, 0.6);
-color: black;
-padding: 5px 23px;
-margin-top: 3%;
-border: none;
-border-radius: 5px;
-cursor: pointer;
-font-size: 12px;
-font-weight: bold;
-box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
-}
-.welcome-box {
-  margin-bottom: 20px;
-}
-.content-box {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  width: 100%;
-}
-.dropdown-content {
-  max-width: 200px;
-  max-height: 200px;
-  margin-top: 10px;
-}
-.icon-choice-box {
-  margin-bottom: 20px;
-}
-.update-profile-box {
-  flex: 1;
-  margin-top: 10px;
-}
-h1 {
-  text-align: center;
-}
-</style>
-
-
-
-
-
-
-
+    methods: {
+      toggleProfileDropdown() {
+        this.showProfileDropdown = !this.showProfileDropdown;
+      },
+      handleAddComic(newComic) {
+        // You can add logic here to update your comic collection with the new comic
+        console.log('New Comic Added:', newComic);
+        // For example, you might want to add it to an array in your data
+        // this.comics.push(newComic);
+      },
+  
+    },
+    components: {
+      // UpdateProfile,
+      ComicCollection,
+      AddComic,
+    },
+  }
+  </script>
+    
+  <style scoped>
+  .add-comic-box {
+    display: flex;
+    justify-content: center;
+    margin-top: -200px;
+  }
+  .comic-collection {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    /* border: solid black; */
+    max-width: 800px;
+    margin: 0 auto;
+  
+  }
+  
+  .main-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    margin-top: -300px;
+  
+  
+  }
+  
+  .comic-username {
+    font-size: 30px;
+    font-weight: bold;
+    font-family: 'Marvel';
+    color: #0c0c0c;
+  }
+  
+  .profile-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  
+    margin-bottom: 200px;
+  }
+  
+  #profileIcon {
+    width: 300px;
+    height: 300px;
+    border-radius: 100%;
+    overflow: hidden;
+  
+  }
+  
+  .profile-icon img {
+    width: 120px;
+  
+  }
+  
+  
+  
+  button {
+    background-color: rgba(255, 0, 0, 0.6);
+    color: black;
+    padding: 5px 23px;
+    margin-top: 3%;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 12px;
+    font-weight: bold;
+    box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  .welcome-box {
+    margin-bottom: 20px;
+    font-family: '';
+  }
+  
+  .content-box {
+    display: flex;
+    flex-direction: column;
+    border: solid;
+    align-items: center;
+    width: auto;
+    align-self: flex-start;
+    margin-top: -100px;
+    min-height: 400px;
+  }
+  
+  .dropdown-content {
+    max-width: 200px;
+    max-height: 200px;
+    margin-top: 10px;
+  
+  }
+  
+  .icon-choice-box {
+    margin-bottom: 20px;
+    cursor: pointer;
+  }
+  
+  .profile-dropdown {
+    position: absolute;
+    padding: 5px;
+  
+  }
+  
+  h1 {
+    text-align: center;
+  }
+  </style>
